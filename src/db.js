8d098036bfef145ee3f16ac4c3ed19517b6f5c9c
giovannaps <giovannapslima.gpsl@gmail.com>
2025-10-26 12:44:55 -0300

    Atualizações no db.js e .env para Vercel

--- src/db.js
+++ src/db.js
@@ -2,10 +2,12 @@ import pg from 'pg';
 import dotenv from 'dotenv';
 dotenv.config();
 
+const isVercel = process.env.VERCEL === '1'; 
+
 export const pool = new pg.Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  user: isVercel ? process.env.SUPABASE_DB_USER : process.env.DB_USER,
+  host: isVercel ? process.env.SUPABASE_DB_HOST : process.env.DB_HOST,
+  database: isVercel ? process.env.SUPABASE_DB_NAME : process.env.DB_NAME,
+  password: isVercel ? process.env.SUPABASE_DB_PASSWORD : process.env.DB_PASSWORD,
+  port: isVercel ? process.env.SUPABASE_DB_PORT : process.env.DB_PORT,
 });
