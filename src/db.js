41c42768f3b84bfb7d02d2c9a32a4fa28cf73e8a
giovannaps <giovannapslima.gpsl@gmail.com>
2025-11-05 22:06:27 -0300

    Adiciona SSL na conexão com supabase

--- src/db.js
+++ src/db.js
@@ -3,11 +3,14 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const isVercel = process.env.VERCEL === '1'; 
+const { Pool } = pg;
 
-export const pool = new pg.Pool({
+export const pool = new Pool({
   user: isVercel ? process.env.SUPABASE_DB_USER : process.env.DB_USER,
   host: isVercel ? process.env.SUPABASE_DB_HOST : process.env.DB_HOST,
   database: isVercel ? process.env.SUPABASE_DB_NAME : process.env.DB_NAME,
   password: isVercel ? process.env.SUPABASE_DB_PASSWORD : process.env.DB_PASSWORD,
   port: isVercel ? process.env.SUPABASE_DB_PORT : process.env.DB_PORT,
+  ssl: isVercel ? { rejectUnauthorized: false } : false, 
 });
+
