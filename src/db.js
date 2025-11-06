11dda81f3e1adb6fcec740e43b61687340ae22b4
giovannaps <giovannapslima.gpsl@gmail.com>
2025-11-06 10:40:58 -0300

    Conectando projeto ao Neon e ajustando env

--- src/db.js
+++ src/db.js
@@ -1,16 +1,12 @@
 import pg from 'pg';
 import dotenv from 'dotenv';
-dotenv.config();
 
-const isVercel = process.env.VERCEL === '1'; 
+dotenv.config();
 const { Pool } = pg;
 
-export const pool = new Pool({
-  user: isVercel ? process.env.SUPABASE_DB_USER : process.env.DB_USER,
-  host: isVercel ? process.env.SUPABASE_DB_HOST : process.env.DB_HOST,
-  database: isVercel ? process.env.SUPABASE_DB_NAME : process.env.DB_NAME,
-  password: isVercel ? process.env.SUPABASE_DB_PASSWORD : process.env.DB_PASSWORD,
-  port: isVercel ? process.env.SUPABASE_DB_PORT : process.env.DB_PORT,
-  ssl: isVercel ? { rejectUnauthorized: false } : false, 
+const pool = new Pool({
+  connectionString: process.env.NEON_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
+export default pool;
